b6ec80d3e2bcae3a3d47a12b38f0c8c77baab0a7
phonghh <longtam20015@gmail.com>
2024-12-07 22:05:25 +0700

    #46 re-build project react

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { createBrowserRouter, Route, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/login.jsx";
+import RegisterPage from "./pages/register.jsx";
+import UserPage from "./pages/user.jsx";
+import ProductPage from "./pages/product.jsx";
+import "./styles/global.css";
 
 const router = createBrowserRouter([
   {
@@ -10,19 +15,19 @@ const router = createBrowserRouter([
   },
   {
     path: "/login",
-    element: <div>Login page</div>,
+    element: <LoginPage />,
   },
   {
     path: "/register",
-    element: <div>Register page</div>,
+    element: <RegisterPage />,
   },
   {
-    path: "/user",
-    element: <div>User page</div>,
+    path: "/users",
+    element: <UserPage />,
   },
   {
-    path: "/product",
-    element: <div>Product page</div>,
+    path: "/products",
+    element: <ProductPage />,
   },
 ]);
 
