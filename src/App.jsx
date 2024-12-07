8d31a9d41e2471908926684b62a4ba157255f456
phonghh <longtam20015@gmail.com>
2024-12-07 13:22:59 +0700

    #29 Import/Export Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,11 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
-const MyComponent = () => {
-  return (
-    <div>
-      <h1>eric & hoidanit</h1>
-    </div>
-  );
-};
+import MyComponent from "./components/learn/MyComponent";
+import {
+  SecondComponent,
+  ThirdComponent,
+} from "./components/learn/SecondComponent";
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -26,6 +23,8 @@ const App = () => {
       </div>
       <h1>Hello world & hoidanit</h1>
       <MyComponent />
+      <SecondComponent />
+      <ThirdComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
