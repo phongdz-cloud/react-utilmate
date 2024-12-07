2bbfa3989f8592bdb2e987fbed2a7e6438d731e9
phonghh <longtam20015@gmail.com>
2024-12-07 13:51:02 +0700

    #31 use variable with JSX

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,12 +1,21 @@
 import "./style.css";
 
 const MyComponent = () => {
+  //   const hoidanit = "eric";
+  //   const hoidanit = 25;
+  //   const hoidanit = true;
+  //   const hoidanit = undefined;
+  const hoidanit = {
+    name: "hoidanit",
+    age: 25,
+  };
   return (
     <>
       <div>
         <h1 className="child" style={{ borderRadius: "10px" }}>
-          eric & hoidanit
+          {JSON.stringify(hoidanit)} & hoidanit
         </h1>
+        <div>{console.log("ERIC")}</div>
       </div>
     </>
   );
