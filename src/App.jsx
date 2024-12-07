a58b11c6b731637e084a2250cc0e4e7616499d8d
phonghh <longtam20015@gmail.com>
2024-12-07 21:17:58 +0700

    #40 Each child key prop

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,25 @@ import reactLogo from "./assets/react.svg";
 import { useState } from "react";
 
 const App = () => {
-  const [todoList, setTodos] = useState([
-    {
-      id: 1,
-      name: "Learning react",
-    },
-    {
-      id: 2,
-      name: "Watching youtube",
-    },
-  ]);
-
-  const hoidanit = "Eric";
-  const age = 25;
-  const data = {
-    address: "Hanoi",
-    country: "vietnam",
-  };
+  const [todoList, setTodos] = useState([]);
 
   const addNewTodo = (name) => {
     const newTodo = {
-      id: todoList.length + 1,
+      id: generateId(),
       name,
     };
     setTodos([...todoList, newTodo]);
+  };
+
+  const generateId = () => {
+    return Math.floor(Math.random() * 1000);
   };
 
   return (
     <div className="todo-container">
       <div className="todo-title">Todo list</div>
       <TodoNew addNewTodo={addNewTodo} />
-      <TodoData name={hoidanit} age={age} data={data} todoList={todoList} />
+      <TodoData todoList={todoList} />
 
       <div className="todo-image">
         <img src={reactLogo} />
