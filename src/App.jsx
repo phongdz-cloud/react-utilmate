544e9171e251d74bbe245a2a88c3e3871649026f
phonghh <longtam20015@gmail.com>
2024-12-07 22:21:32 +0700

    #48 Nested routes with outlet

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import reactLogo from "./assets/react.svg";
 import { useState } from "react";
 import Header from "./components/layout/ header";
 import Footer from "./components/layout/footer";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   const [todoList, setTodos] = useState([]);
@@ -41,6 +42,7 @@ const App = () => {
           <TodoData todoList={todoList} deleteTodo={deleteTodo} />
         )}
       </div>
+      <Outlet />
       <Footer />
     </>
   );
