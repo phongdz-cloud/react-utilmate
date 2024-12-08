ed8120848c3b362513adb95b7e83df8e0e35da08
phonghh <longtam20015@gmail.com>
2024-12-08 22:37:55 +0700

    #74 design modal create user

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,4 +1,4 @@
-import { Space, Table, Tag } from "antd";
+import { Table } from "antd";
 import { fetchAllUserAPI } from "../../services/api.service";
 import { useEffect, useState } from "react";
 
@@ -24,29 +24,6 @@ const UserTable = () => {
       dataIndex: "email",
     },
   ];
-  //   const data = [
-  //     {
-  //       key: "1",
-  //       name: "John Brown",
-  //       age: 32,
-  //       address: "New York No. 1 Lake Park",
-  //       tags: ["nice", "developer"],
-  //     },
-  //     {
-  //       key: "2",
-  //       name: "Jim Green",
-  //       age: 42,
-  //       address: "London No. 1 Lake Park",
-  //       tags: ["loser"],
-  //     },
-  //     {
-  //       key: "3",
-  //       name: "Joe Black",
-  //       age: 32,
-  //       address: "Sydney No. 1 Lake Park",
-  //       tags: ["cool", "teacher"],
-  //     },
-  //   ];
 
   const loadUser = async () => {
     const res = await fetchAllUserAPI();
