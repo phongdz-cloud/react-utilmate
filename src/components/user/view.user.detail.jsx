741c821e8821819715568e3fbc274c174f6973bd
phonghh <longtam20015@gmail.com>
2024-12-09 22:33:18 +0700

    #79 Detail User

---
+++ src/components/user/view.user.detail.jsx
@@ -0,0 +1,26 @@
+import { Drawer } from "antd";
+import { useState } from "react";
+
+const ViewUserDetail = (props) => {
+  const { isViewDrawerOpen, setIsViewDrawerOpen, dataDetail, setDataDetail } =
+    props;
+
+  const onClose = () => {
+    console.log("check props ", isViewDrawerOpen);
+    setIsViewDrawerOpen(false);
+    setDataDetail(null);
+  };
+
+  return (
+    <>
+      <Drawer title="Basic Drawer" onClose={onClose} open={isViewDrawerOpen}>
+        <p>Id: {dataDetail?._id}</p>
+        <p>Full name: {dataDetail?.fullName}</p>
+        <p>Email: {dataDetail?.email}</p>
+        <p>PhoneNumber: {dataDetail?.phone}</p>
+      </Drawer>
+    </>
+  );
+};
+
+export default ViewUserDetail;
