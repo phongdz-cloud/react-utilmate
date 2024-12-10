5ade5eb7e74181f463924eab0821a6a135faa108
phonghh <longtam20015@gmail.com>
2024-12-10 22:45:39 +0700

    Uncontrolled component for register

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,5 +1,80 @@
+import { Button, Checkbox, Form, Input } from "antd";
+
 const RegisterPage = () => {
-  return <div>Register page</div>;
+  const [form] = Form.useForm();
+
+  const onFinish = (value) => {
+    console.log("value", value);
+  };
+
+  return (
+    <Form
+      layout="vertical"
+      form={form}
+      onFinish={onFinish}
+      // onFinishFailed={onFinishFailed}
+    >
+      <div
+        style={{
+          margin: "50px",
+        }}
+      >
+        <Form.Item
+          label="Full Name"
+          name="fullName"
+          // rules={[
+          //   {
+          //     required: true,
+          //     message: "Please input your username!",
+          //   },
+          // ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Email"
+          name="email"
+          // rules={[
+          //   {
+          //     required: true,
+          //     message: "Please input your username!",
+          //   },
+          // ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          // rules={[
+          //   {
+          //     required: true,
+          //     message: "Please input your username!",
+          //   },
+          // ]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item
+          label="Phone number"
+          name="phone"
+          // rules={[
+          //   {
+          //     required: true,
+          //     message: "Please input your username!",
+          //   },
+          // ]}
+        >
+          <Input />
+        </Form.Item>
+        <div>
+          <Button onClick={() => form.submit()} type="primary">
+            Register
+          </Button>
+        </div>
+      </div>
+    </Form>
+  );
 };
 
 export default RegisterPage;
